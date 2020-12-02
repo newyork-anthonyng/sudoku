b4bac8e9944c3a9c9b2060d838d4ffd288816cde
Anthony Ng <newyork.anthonyng@gmail.com>
2020-12-02 17:43:01 -0500

    highlight rows and columns

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# Sudoku API
+http://www.cs.utep.edu/cheon/ws/sudoku/
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import React from "react";
 import apiData from "./api";
 import "./App.css";
+import classNames from "classnames";
 
 const emptyGrid = new Array(9);
 for (let i = 0; i < emptyGrid.length; i++) {
   emptyGrid[i] = new Array(9);
-  emptyGrid[i].fill({ filled: false, value: undefined });
+
+  for (let j = 0; j < emptyGrid[i].length; j++) {
+    emptyGrid[i][j] = { filled: false, value: undefined, x: i, y: j };
+  }
+}
+
+function NumberPicker(props) {
+  const handleClick = (number) => () => {
+    props.onSelect(number);
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick(1)}>1</button>
+      <button onClick={handleClick(2)}>2</button>
+      <button onClick={handleClick(3)}>3</button>
+      <button onClick={handleClick(4)}>4</button>
+      <button onClick={handleClick(5)}>5</button>
+      <button onClick={handleClick(6)}>6</button>
+      <button onClick={handleClick(7)}>7</button>
+      <button onClick={handleClick(8)}>8</button>
+      <button onClick={handleClick(9)}>9</button>
+    </div>
+  );
+}
+
+function isSquareHighlighted(
+  { x: selectedSquareX, y: selectedSquareY } = {},
+  { x: currentSquareX, y: currentSquareY } = {}
+) {
+  return (
+    selectedSquareX === currentSquareX || selectedSquareY === currentSquareY
+  );
 }
 
 function App() {
@@ -14,6 +47,7 @@ function App() {
       const { x, y, value } = square;
 
       emptyGrid[x][y] = {
+        ...emptyGrid[x][y],
         filled: true,
         value: value,
       };
@@ -21,34 +55,49 @@ function App() {
 
     return emptyGrid;
   });
+  const [selectedSquare, setSelectedSquare] = React.useState();
 
-  const handleNumberChange = (rowIndex, cellIndex) => (event) => {
-    const newGrid = data.slice();
-    newGrid[rowIndex] = newGrid[rowIndex].slice();
-    newGrid[rowIndex][cellIndex] = {
-      ...newGrid[rowIndex][cellIndex],
-      value: event.target.value,
-    };
-    setData(newGrid);
+  const handleNumberSelect = (number) => {
+    console.log(number);
   };
+
+  const handleEmptyCellClick = ({ x, y }) => () => {
+    setSelectedSquare({ x, y });
+  };
+
+  console.log(selectedSquare);
 
   return (
     <div className="container">
       {data.map((row, rowIndex) => {
         return (
           <div className="row" key={rowIndex}>
-            {row.map((cell, cellIndex) => {
+            {row.map(({ value, filled, x, y }, cellIndex) => {
+              const isHighlighted = isSquareHighlighted(selectedSquare, {
+                x,
+                y,
+              });
+
+              const squareClass = classNames("square", {
+                "square--highighted": isHighlighted,
+              });
+
               return (
-                <div className="square" key={cellIndex}>
-                  {cell.filled ? (
-                    cell.value
+                <div className={squareClass} key={cellIndex}>
+                  {filled ? (
+                    <div
+                      className="square__input--filled"
+                      onClick={handleEmptyCellClick({ x, y })}
+                    >
+                      {value}
+                    </div>
                   ) : (
-                    <input
-                      className="square__input"
-                      type="number"
-                      onChange={handleNumberChange(rowIndex, cellIndex)}
-                      value={cell.value || ""}
-                    />
+                    <div
+                      className="square__input--unfilled"
+                      onClick={handleEmptyCellClick({ x, y })}
+                    >
+                      {value || " "}
+                    </div>
                   )}
                 </div>
               );
@@ -56,6 +105,7 @@ function App() {
           </div>
         );
       })}
+      <NumberPicker onSelect={handleNumberSelect} />
     </div>
   );
 }
